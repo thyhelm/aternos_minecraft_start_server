ba95dd18a9fb65803e336d1427574a50a42c3543
thyhelm <Dawid.Cieslak@Live.umcs.edu.pl>
2020-08-05 17:25:11 +0200

    powershele

---
+++ get_splinter.ps1
@@ -0,0 +1,5 @@
+git clone git://github.com/cobrateam/splinter.git
+cd splinter
+python setup.py install 
+cd ..
+Remove-Item splinter -recurse -force
--- start_server.py
+++ start_server.py
@@ -1,6 +1,15 @@
 from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-with Browser() as browser:
+executable_path = {'executable_path': ChromeDriverManager().install()}
+
+with Browser('chrome', **executable_path) as browser:
     url = 'https://aternos.org/go/'
     browser.visit(url)
+    browser.find_by_id('user').fill('')
+    browser.find_by_id('password').fill('')
+    browser.find_by_id('login').click()
+    browser.visit('https://aternos.org/server/')
+    time.sleep(20)
     
